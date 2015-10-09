178d011dd1ed2918ce3e34506dc626446ca4cf06
Songchun Fan <schfan@cs.duke.edu>
2015-10-09 19:07:04 -0400

    Added scripts

--- gem5-script/run_android_bbench.sh
+++ gem5-script/run_android_bbench.sh
@@ -2,4 +2,4 @@
 
 export M5_PATH=../system/Android/
 
-./build/ARM/gem5.opt --outdir=m5out-bbench --stats-file=/scratch/sf117/bbench.txt configs/example/fs.py -b bbench-ics --kernel=vmlinux.smp.mouse.arm --num-cpu=1 --caches --l1d_size=32kB --l1i_size=32kB --l2cache --l2_size=512kB --cpu-type=detailed  --fetchWidth=3 --decodeWidth=3 --renameWidth=3 --dispatchWidth=3 --issueWidth=3 --wbWidth=3 --commitWidth=3 --squashWidth=3 --l1_writethrough=1 --l2_writethrough=0 --dump-stats=1000 -r 1 
+./build/ARM/gem5.opt --outdir=m5out-bbench --stats-file=bbench.txt configs/example/fs.py -b bbench-ics --kernel=vmlinux.smp.mouse.arm --num-cpu=1 --caches --l1d_size=32kB --l1i_size=32kB --l2cache --l2_size=512kB --cpu-type=detailed  --fetchWidth=3 --decodeWidth=3 --renameWidth=3 --dispatchWidth=3 --issueWidth=3 --wbWidth=3 --commitWidth=3 --squashWidth=3 --l1_writethrough=1 --l2_writethrough=0 --dump-stats=1000 -r 1 
--- gem5-script/run_android_sm.sh
+++ gem5-script/run_android_sm.sh
@@ -2,5 +2,5 @@
 
 export M5_PATH=../system/AndroidHelloWorld/
 
-#./build/ARM/gem5.opt --outdir=m5out-sm --stats-file=/scratch/sf117/sm_io.txt configs/example/fs.py -b android-sm --kernel=vmlinux.smp.mouse.arm --num-cpu=1 --caches --l1d_size=32kB --l1i_size=32kB --l2cache --l2_size=512kB --cpu-type=detailed  --fetchWidth=3 --decodeWidth=3 --renameWidth=3 --dispatchWidth=3 --issueWidth=3 --wbWidth=3 --commitWidth=3 --squashWidth=3 --dump-stats=1000 -r 1 
+#./build/ARM/gem5.opt --outdir=m5out-sm --stats-file=sm_io.txt configs/example/fs.py -b android-sm --kernel=vmlinux.smp.mouse.arm --num-cpu=1 --caches --l1d_size=32kB --l1i_size=32kB --l2cache --l2_size=512kB --cpu-type=detailed  --fetchWidth=3 --decodeWidth=3 --renameWidth=3 --dispatchWidth=3 --issueWidth=3 --wbWidth=3 --commitWidth=3 --squashWidth=3 --dump-stats=1000 -r 1 
 ./build/ARM/gem5.opt --outdir=m5out-sm configs/example/fs.py -b android-sm --kernel=vmlinux.smp.mouse.arm --num-cpu=1 --caches --l1d_size=32kB --l1i_size=32kB --l2cache --l2_size=512kB --cpu-type=detailed  --fetchWidth=3 --decodeWidth=3 --renameWidth=3 --dispatchWidth=3 --issueWidth=3 --wbWidth=3 --commitWidth=3 --squashWidth=3 -r 1 
